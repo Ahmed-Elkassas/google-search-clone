5fd42cdf9178593c08139d669b485150f095b5d3
Ahmed-Basuony <ahmed.basouny@awstreams.com>
2022-08-20 16:35:16 +0200

    Display Fetched Data for search pathname for testing

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -17,7 +17,17 @@ export const Results = () => {
 
   switch (location.pathname) {
     case '/search':
-      return 'Searching'
+      return (
+        <div className='flex flex-wrap justify-between items-center space-y-6 sm:px-56 px-4'> 
+          {results?.results?.map(({link, title}, index) => (
+            <div key={index} className="md:w-2/5 w-full"> 
+              <a href={link} target="_blank" rel='noreferrer'>
+                <p className='text-sm'>{link.length > 30 ? link.substring(0, 30) : link}</p>
+                <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>{title}</p>
+              </a>
+            </div>
+          ))}
+        </div>);
     case '/images':
       return 'images'
   
